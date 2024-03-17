a86e8743447273fef56c650058e3c3502da7ce3f
Дмитрий Евреинов <d.evreinov@outlook.com>
2024-03-17 18:49:32 +0300

    fix: :bug: sprint2_final.

--- src/Sprint2_final/B/Program.cs
+++ src/Sprint2_final/B/Program.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * Делаем все согласно описанному алгоритму в задаче.
- * Единственное, я не понимаю каким образом можно сделать округление вниз
- * без использования Math.Floor().
+ * Реализовали собственный стек.
+ * Сложность алгоритма O(n), где n количество элементов в массиве.
+ * Извлечением двуч чисел из стека во время вычисления, думаю можно принебречь.
  */
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 
 public class Program
@@ -16,7 +16,7 @@ public class Program
 
     public static int Calculate(string[] array)
     {
-        _stack = new Stack<int>(array.Length);
+        _stack = new Stack<int>();
         foreach (var str in array)
         {
             switch (str)
@@ -96,5 +96,71 @@ public class Program
 
         _reader.Close();
         _writer.Close();
+    }
+}
+
+/// <summary>
+/// Небольшой самописный стек.
+/// </summary>
+/// <typeparam name="TValue">Тип хранимого элемента.</typeparam>
+public class Stack<TValue>
+{
+    private Node<TValue>? head = null;
+
+    /// <summary>
+    /// Дробавить элемент.
+    /// </summary>
+    /// <param name="value">Элемент.</param>
+    public void Push(TValue value)
+    {
+        var node = new Node<TValue>(value, null);
+        if (head != null)
+        {
+            node.Next = head;
+        }
+        head = node;
+    }
+
+    /// <summary>
+    /// Извлечь элемент.
+    /// </summary>
+    /// <returns>Элемент.</returns>
+    public TValue Pop()
+    {
+        if (head is null)
+        {
+            throw new InvalidOperationException($"Стек {nameof(Stack<TValue>)} является пустым.");
+        }
+        var value = head.Value;
+        head = head.Next;
+        return value;
+    }
+}
+
+/// <summary>
+/// Элемент Stack.
+/// </summary>
+/// <typeparam name="TValue">Тип значения хранимого в стеке.</typeparam>
+public class Node<TValue>
+{
+    /// <summary>
+    /// Значение.
+    /// </summary>
+    public TValue Value { get; set; }
+
+    /// <summary>
+    /// Следующий элемент стека.
+    /// </summary>
+    public Node<TValue>? Next { get; set; }
+
+    /// <summary>
+    /// Создать элемент Stack.
+    /// </summary>
+    /// <param name="value">Значение.</param>
+    /// <param name="next">Следующий элемент.</param>
+    public Node(TValue value, Node<TValue>? next)
+    {
+        Value = value;
+        Next = next;
     }
 }
